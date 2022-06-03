f0e91b3ba60d705549b9601e435a9d5ac7127004
Samuel <smarouba2@gmail.com>
2022-06-03 09:02:14 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide =(parameter1, parmater2) => parameter1 / parmater2;
+// divide(2000,100)
+
+const square = x => x * x
+// square is th function name
+// x is the parameter being passed in tha is set to 2
+
+const add = (a, b) => a + b;
+// add is the function, 
+// a + b are parmeter bing passed where (a) = 3 and (b) = 4
+
